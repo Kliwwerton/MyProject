b03fb1ad04fd8d63e11b24569d0a63c08098c8ec
Alexander <AStrukov-96@yandex.ru>
2023-02-07 08:15:50 +0500

    Added new TextInput and Label (Удельное давление, значение)

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-
 from kivy.config import Config
+
 Config.set('graphics', 'width', 400)
 Config.set('graphics', 'height', 700)
 
 from kivy.app import App
 from kivy.core.window import Window
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.boxlayout import BoxLayout
-# from kivy.uix.textinput import TextInput
 
 
 class Container(ScreenManager):
-
     pass
 
 
@@ -31,7 +26,7 @@ class Second(Screen):
 
 class EngineerApp(App):
     def build(self):
-        Window.clearcolor = (0.91,.72,.99,.5)
+        Window.clearcolor = (0.91, .72, .99, .5)
         container = Container()
         container.add_widget(First())
         container.add_widget(Second())
