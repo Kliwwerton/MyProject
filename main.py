59e9b7becf73c306818e3cdcacaca497ef7080d7
AStru <AStrukov-96@yandex.ru>
2023-02-08 22:04:37 +0500

    Added new function RESET for button reset. Added widgets pnt for button reset.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 
+
 Builder.load_file('Engineer.kv')
 
 
@@ -26,9 +27,11 @@ class First(Screen):
 
 class Second(Screen):
     def press(self):
-        self.ids.label_P_pressing_text.text = 'Давление прессования: '
+        self.ids.label_P_pressing_text.text = 'Давление прессования:'
+        self.ids.label_P_pressing_value.text = 'VALUE'
+        self.ids.unit_of_measurement.text = 'Пока NONE'
 
-    def unpressed(self):
+    def calculate(self):
         pass
 
     def press_exit(self):
@@ -37,10 +40,27 @@ class Second(Screen):
         else:
             self.ids.My_image.source = 'Images/exit.png'
 
+    def reset(self):
+        if self.ids.image_btn_reset.source == 'Images/reset.png':
+            self.ids.image_btn_reset.source = 'Images/reset_2.png'
+        else:
+            self.ids.image_btn_reset.source = 'Images/reset.png'
+        self.ids.label_P_pressing_text.text = ''
+        self.ids.label_P_pressing_value.text = ''
+        self.ids.unit_of_measurement.text = ''
+        self.ids.spinner_quantity_stamps.text = '0'
+        self.ids.spinner_id.text = 'Выбурите пресс!'
+        self.ids.text_input_Pud.text = ''
+        self.ids.label_S_pressing_value.text = ''
+
+    def reset_2(self):
+        self.ids.image_btn_reset.source = 'Images/reset.png'
+
+
 
 class EngineerApp(App):
     def build(self):
-        Window.clearcolor = (0.91, .72, .99, .5)
+        Window.clearcolor = (232/255, 184/255, 1, 1)
         container = Container()
         container.add_widget(First())
         container.add_widget(Second())
