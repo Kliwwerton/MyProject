b02b912c556373f6be592e7d144a1f327d62d5d3
Alexander <AStrukov-96@yandex.ru>
2023-02-10 08:36:47 +0500

    Create new function (calculate_specific_pressure)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivy.config import Config
 
-Config.set('graphics', 'width', 576)
-Config.set('graphics', 'height', 1024)
+# Config.set('graphics', 'width', 576)
+# Config.set('graphics', 'height', 1024)
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
 from kivy.app import App
@@ -9,7 +9,6 @@ from kivy.core.window import Window
 from kivy.core.audio import SoundLoader
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
-
 
 Builder.load_file('Engineer.kv')
 
@@ -30,12 +29,22 @@ PRESS_PARAMETERS = {'LAEIS-1250': [1250, 11.98, 'N/см[sup]2[/sup]'],
 def calculate_pressure(press, square_pressing, quantity_stamps, specific_pressure):
     if press in PRESS_PARAMETERS:
         pressure = (square_pressing * quantity_stamps * PRESS_PARAMETERS[press][1] *
-                    (specific_pressure/1000)) / PRESS_PARAMETERS[press][0]
+                    (specific_pressure / 1000)) / PRESS_PARAMETERS[press][0]
         values = [round(pressure, 2), PRESS_PARAMETERS[press][2]]
         return values
     else:
         pressure = False
         return pressure
+
+
+def calculate_specific_pressure(press, square_pressing, quantity_stamps, pressure):
+    data = []
+    return data
+
+
+def return_label(item):
+    value = PRESS_PARAMETERS[item][2]
+    return value
 
 
 class Container(ScreenManager):
@@ -88,12 +97,53 @@ class Second(Screen):
 
 
 class Third(Screen):
-    pass
+    sound = SoundLoader.load('sounds/sound.wav')
+    sound_reset = SoundLoader.load('sounds/sound_reset.mp3')
+
+    def calculate(self):
+        self.sound.play()
+        if self.ids.spinner_quantity_stamps.text == '0':
+            self.press()
+        elif self.ids.spinner_press_mark.text \
+                and self.ids.label_S_pressing_value.text \
+                and self.ids.spinner_quantity_stamps.text != '0' \
+                and self.ids.specific_pressure.text:
+            data = calculate_specific_pressure(press=self.ids.spinner_press_mark.text,
+                                               square_pressing=float(self.ids.label_S_pressing_value.text),
+                                               quantity_stamps=int(self.ids.spinner_quantity_stamps.text),
+                                               pressure=int(self.ids.specific_pressure.text))
+            if not data:
+                self.press()
+            else:
+                self.ids.label_P_pressing_text.text = 'Удельное давление:'
+                # self.ids.label_P_pressing_value.text = str(data[0])
+                # self.ids.unit_of_measurement.text = str(data[1])
+        else:
+            self.press()
+
+    def press(self):
+        self.ids.label_P_pressing_text.text = 'НЕХВАТАЕТ ДАННЫХ!!!'
+        self.ids.label_P_pressing_value.text = ''
+        self.ids.unit_of_measurement.text = ''
+
+    def reset(self):
+        self.ids.label_P_pressing_text.text = ''
+        self.ids.label_P_pressing_value.text = ''
+        self.ids.unit_of_measurement.text = ''
+        self.ids.spinner_quantity_stamps.text = '0'
+        self.ids.spinner_press_mark.text = 'Выберите пресс!'
+        self.ids.specific_pressure.text = ''
+        self.ids.label_S_pressing_value.text = ''
+        self.sound_reset.play()
+
+    def change_label(self):
+        self.ids.specific_pressure_unit.text = return_label(self.ids.spinner_press_mark.text)
 
 
 class EngineerApp(App):
     def build(self):
-        Window.clearcolor = (232/255, 184/255, 1, 1)
+        Window.clearcolor = (232 / 255, 184 / 255, 1, 1)
+        Window.fullscreen = 'auto'
         container = Container()
         container.add_widget(First())
         container.add_widget(Second())
