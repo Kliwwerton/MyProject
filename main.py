dc1c6d9c313354f8933f95749c05898ef91a10a6
Alexander <AStrukov-96@yandex.ru>
2023-02-15 16:47:13 +0500

    Added GOST_STANDARDS

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.config import Config
 Config.set('graphics', 'width', 360)
 Config.set('graphics', 'height', 820)
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
+# Config.set('kivy', 'window_icon', 'Images/Logo.png')
 
 from kivy.app import App
 from kivy.core.window import Window
@@ -33,13 +34,13 @@ def calculate_pressure(press, square_pressing, quantity_stamps, specific_pressur
         return pressure
 
 
-def calculate_specific_pressure(press, square_pressing, quantity_stamps, pressure):
-    if pressure > PRESS_PARAMETERS[press][1]:
-        return 'ДАВЛЕНИЕ ПРЕССА ПРЕВЫШЕНО!!!'
-    else:
-        specific_pressure = (pressure * PRESS_PARAMETERS[press][0]) / \
-                        (square_pressing * quantity_stamps * PRESS_PARAMETERS[press][1])
-        return round(specific_pressure * 1000, 2)
+# def calculate_specific_pressure(press, square_pressing, quantity_stamps, pressure):
+#     if pressure > PRESS_PARAMETERS[press][1]:
+#         return 'ДАВЛЕНИЕ ПРЕССА ПРЕВЫШЕНО!!!'
+#     else:
+#         specific_pressure = (pressure * PRESS_PARAMETERS[press][0]) / \
+#                         (square_pressing * quantity_stamps * PRESS_PARAMETERS[press][1])
+#         return round(specific_pressure * 1000, 2)
 
 
 def return_label(item):
@@ -72,10 +73,10 @@ class Second(Screen):
                 and self.ids.label_S_pressing_value.text \
                 and self.ids.spinner_quantity_stamps.text != '0' \
                 and self.ids.specific_pressure.text:
-            data = calculate_pressure(press=self.ids.spinner_press_mark.text,
-                                      square_pressing=float(self.ids.label_S_pressing_value.text),
-                                      quantity_stamps=int(self.ids.spinner_quantity_stamps.text),
-                                      specific_pressure=int(self.ids.specific_pressure.text))
+            data = self.calculate_pressure(press=self.ids.spinner_press_mark.text,
+                                           square_pressing=float(self.ids.label_S_pressing_value.text),
+                                           quantity_stamps=int(self.ids.spinner_quantity_stamps.text),
+                                           specific_pressure=int(self.ids.specific_pressure.text))
             if not data:
                 self.press()
             else:
@@ -84,6 +85,20 @@ class Second(Screen):
                 self.ids.unit_of_measurement.text = str(data[1])
         else:
             self.press()
+
+    @staticmethod
+    def calculate_pressure(press, square_pressing, quantity_stamps, specific_pressure):
+        if press in PRESS_PARAMETERS:
+            pressure = (square_pressing * quantity_stamps * PRESS_PARAMETERS[press][1] *
+                        (specific_pressure / 1000)) / PRESS_PARAMETERS[press][0]
+            if pressure > PRESS_PARAMETERS[press][1]:
+                values = ['ПРЕСС СТОЛЬКО', 'НЕ ПОТЯНЕТ!!!']
+            else:
+                values = [round(pressure, 2), PRESS_PARAMETERS[press][2]]
+            return values
+        else:
+            pressure = False
+            return pressure
 
     def reset(self):
         self.ids.label_P_pressing_text.text = ''
@@ -110,10 +125,10 @@ class Third(Screen):
                 and self.ids.S_pressing_value.text \
                 and self.ids.spinner_quantity_stamps.text != '0' \
                 and self.ids.pressure.text:
-            data = calculate_specific_pressure(press=self.ids.spinner_press_mark.text,
-                                               square_pressing=float(self.ids.S_pressing_value.text),
-                                               quantity_stamps=int(self.ids.spinner_quantity_stamps.text),
-                                               pressure=float(self.ids.pressure.text))
+            data = self.calculate_specific_pressure(press=self.ids.spinner_press_mark.text,
+                                                    square_pressing=float(self.ids.S_pressing_value.text),
+                                                    quantity_stamps=int(self.ids.spinner_quantity_stamps.text),
+                                                    pressure=float(self.ids.pressure.text))
             if not data:
                 self.press()
             elif isinstance(data, str):
@@ -126,6 +141,15 @@ class Third(Screen):
                 self.ids.unit_of_measurement.text = 'кг/см[sup]2[/sup]'
         else:
             self.press()
+
+    @staticmethod
+    def calculate_specific_pressure(press, square_pressing, quantity_stamps, pressure):
+        if pressure > PRESS_PARAMETERS[press][1]:
+            return 'ДАВЛЕНИЕ ПРЕССА ПРЕВЫШЕНО!!!'
+        else:
+            specific_pressure = (pressure * PRESS_PARAMETERS[press][0]) / \
+                                (square_pressing * quantity_stamps * PRESS_PARAMETERS[press][1])
+            return round(specific_pressure * 1000, 2)
 
     def press(self):
         self.ids.label_P_pressing_text.text = 'НЕХВАТАЕТ ДАННЫХ!!!'
@@ -151,6 +175,7 @@ class Third(Screen):
 
 class EngineerApp(App):
     def build(self):
+        self.icon = 'Images/Logo.png'
         Window.clearcolor = (232 / 255, 184 / 255, 1, 1)
         container = Container()
         container.add_widget(First())
@@ -160,5 +185,4 @@ class EngineerApp(App):
 
 
 if __name__ == '__main__':
-    EngineerApp(title='ИНЖЕНЕР НА ВСЮ ГОЛОВУ!',
-                icon='Images/Logo.png').run()
+    EngineerApp(title='ИНЖЕНЕР НА ВСЮ ГОЛОВУ!').run()
