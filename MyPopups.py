1a85106b6f5836c458ca9cd72298ccfa9dbda313
AStru <AStrukov-96@yandex.ru>
2023-02-18 00:08:49 +0500

    Modification MyPopups.kv (CalculationRectangle)

--- MyPopups.py
+++ MyPopups.py
@@ -20,12 +20,26 @@ class SelectionGostPopup(Popup):
     def return_beck():
         SelectionOptionPopup().open()
 
-    @staticmethod
-    def opening_calculation_window():
-        CalculationsAreaOfRectangle().open()
+    def opening_calculation_window(self):
+        popup = CalculationsAreaOfRectangle()
+        popup.chose_values.append(self.ids.gost_number.text)
+        popup.chose_values.append(self.ids.product_number.text)
+        popup.open()
+
 
 
 class CalculationsAreaOfRectangle(Popup):
+    def __init__(self):
+        super().__init__()
+        self.chose_values = []
+
+    def build_instance(self):
+        self.ids.label_gost_number.text = self.chose_values[0] + ' № ' + self.chose_values[1]
+        product_size = GOST_STANDARDS[self.chose_values[0]][self.chose_values[1]]
+        self.ids.length_value.text = str(product_size[0])
+        self.ids.width_value.text = str(product_size[1])
+        self.ids.thickness_value.text = str(product_size[2])
+
     @staticmethod
     def return_beck():
         SelectionOptionPopup().open()
